cb42fdbff71f137181b2745faf8d3ca2d472418a
JacklineCharles <wanjirajackie8@gmail.com>
2017-03-07 13:50:30 +0300

    Add files via upload
    This consists of fizzbuzz code that checks the divisibility of a number by 3,5 or both and its test

---
+++ fizzbuzz.py
@@ -0,0 +1,16 @@
+def fizz_buzz(num):#creating a function called fizz_buzz
+    if isinstance(num, int):#checking whether variable num is an integer	
+  
+        if num%15 == 0:#if the number is divisible by 3 and 5,print FizzBuzz 
+            return ('FizzBuzz')
+
+        elif num%3==0:#if the number is divisible by 3,print Fizz 
+            return ('Fizz')
+
+        elif num%5==0:#if the number is divisible by 5,print Buzz 
+           return ('Buzz')
+
+        else: #if the number is not divisible by either 3 and 5,return the number
+            return num
+  
+fizz_buzz(54)
